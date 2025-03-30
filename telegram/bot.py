310172066e5ae97c95dddc2fb60162cf6aad51b2
Tikhon <tihonsergienko@yandex.ru>
2025-03-31 00:12:51 +0300

    add qr codes

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,7 +1,9 @@
 import logging
 import requests
+import qrcode
+import io
 import os
-from telegram import Update, ReplyKeyboardRemove
+from telegram import Update, InputMediaPhoto
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -11,25 +13,40 @@ from telegram.ext import (
     ConversationHandler,
 )
 
-API_URL = "http://snaplink-service:8080/api/v1/shorten"
+INTERNAL_API_URL = "http://snaplink-service:8080/api/v1/shorten"
+EXTERNAL_API_URL = os.getenv("EXTERNAL_API_URL", default="http://localhost:8080")
 
 logging.basicConfig(level=logging.INFO)
 
 
 def shorten_url(url: str) -> str:
-    response = requests.post(API_URL, json={"link": url})
+    response = requests.post(INTERNAL_API_URL, json={"link": url})
     if response.status_code == 200:
         data = response.json()
-        return f"http://localhost:8080/t/{data['id']}"
+        return f"{EXTERNAL_API_URL}/t/{data['id']}"
     else:
         return "Ошибка при сокращении ссылки. Попробуйте позже."
+
+
+def generate_qr_code(url: str) -> io.BytesIO:
+    qr = qrcode.make(url)
+    img_bytes = io.BytesIO()
+    qr.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+    return img_bytes
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message_text = update.message.text
     if message_text.startswith("http://") or message_text.startswith("https://"):
         short_link = shorten_url(message_text)
-        await update.message.reply_text(short_link)
+        qr_code = generate_qr_code(short_link)
+        await update.message.reply_media_group(
+            media=[
+                InputMediaPhoto(qr_code, caption=short_link)
+            ],
+            reply_to_message_id=update.message.message_id
+        )
     else:
         await update.message.reply_text("Пожалуйста, отправьте корректную ссылку.")
 
